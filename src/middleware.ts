e00f7733a01266dfc8e1cb3e5bb0f770c6e57638
Kuldeep <kuldeepmer11@gmail.com>
2024-03-29 18:04:29 +0530

    supabase-initialized

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "./lib/supabase/supabaseServer";//as it is sever file so import from there
+import { cookies } from "next/headers";
+
+
+export async function middleware(request: NextRequest){
+    const supabase = createClient(cookies())
+    const {data } = await supabase.auth.getUser();
+    if(data == null){
+        return NextResponse.redirect(new URL("/login?error=Please login first to acces this route." , request.url));
+    }
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher : []
+}
